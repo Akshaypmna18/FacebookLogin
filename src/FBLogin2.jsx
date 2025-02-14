be14af27b0f87b1e3c61d3d7911b9c537d2505ae
Aкѕнαу <67232475+Akshaypmna18@users.noreply.github.com>
2025-02-14 15:13:19 +0530

    Update FBLogin2.jsx

--- src/FBLogin2.jsx
+++ src/FBLogin2.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from "react";
 
 const FacebookBusinessLogin = () => {
@@ -29,38 +28,39 @@ const handleLoginClick = () => {
     return;
   }
 
+  const redirectUri = encodeURIComponent("https://app.admini.co.in/dashboard/settings?tab=facebookLogin2");
+
   FB.login(
     function (response) {
       console.log("Facebook Login Response:", response);
 
       if (response.authResponse) {
         const authCode = response.authResponse.code;
-        
-        const appId = '2525589477642277';
-        const redirectUri = 'https://app.admini.co.in/dashboard/settings?tab=facebookLogin2';
-        const appSecret = '5e5dc837d3b9c485981b088b1f8fdf33';
 
-        
+        const appId = "2525589477642277";
+        const appSecret = "5e5dc837d3b9c485981b088b1f8fdf33";
+
         const url = `https://graph.facebook.com/v20.0/oauth/access_token?client_id=${appId}&redirect_uri=${redirectUri}&client_secret=${appSecret}&code=${authCode}`;
 
         fetch(url)
-          .then(response => response.json())
-          .then(data => console.log('Response:', data))
-          .catch(error => console.error('Error:', error));
+          .then((response) => response.json())
+          .then((data) => console.log("Response:", data))
+          .catch((error) => console.error("Error:", error));
       }
     },
     {
       config_id: "1121290476363268",
-      response_type: "code",  
+      response_type: "code",
       override_default_response_type: true,
+      auth_type: "rerequest",
+      redirect_uri: "https://app.admini.co.in/dashboard/settings?tab=facebookLogin2",
     }
   );
 };
 
-
   return (
     <div>
-<button  onClick={handleLoginClick}>Login with Facebook business</button>
+      <button onClick={handleLoginClick}>Login with Facebook business</button>
     </div>
   );
 };
